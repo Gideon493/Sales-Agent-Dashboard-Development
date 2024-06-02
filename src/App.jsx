d8f835b8d102d7592f8ebe66e4e913e86f79d335
Gideon93 <gideongetich493@gmail.com>
2024-06-02 16:58:19 +0300

    First project commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const App = () => {
+  
+
+  return (
+    <div className='p-4 bg-slate-200 h-svh'>
+     
+     <h3 className='text-xl text-slate-900'>Hello Sales Agent Dashboard</h3>
+   
+    </div>
+  )
+}
+
+export default App
